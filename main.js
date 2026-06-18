66bf2f7f5651743a59ae93c27fdbc95d8f6d8656
strawberryskeleton <xradhi5685@gmail.com>
2026-06-18 21:39:17 +0530

    added main evaluation logic

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 
 const SECRET_PIN = '1234'
+let attemptsLeft = 10
+
 const outputLog = document.getElementById('output-log')
 const userInput = document.getElementById('user-input')
 
@@ -13,7 +15,7 @@ userInput.addEventListener("keydown", (ev) => {
         let guess = userInput.value
         userInput.value = ''
 
-        if (guess.length != 4) {
+        if (guess.length != 4 || isNaN(guess)) {
             // console.log('error')
             printToScreen('error')
             return
@@ -27,6 +29,8 @@ userInput.addEventListener("keydown", (ev) => {
             // console.log('try again')
             printToScreen('try again')
         }
+
+        evaluateGuess(guess)
     }
 })
 
@@ -43,3 +47,47 @@ function initaliseGame () {
 }
 
 initaliseGame()
+
+function evaluateGuess (guess) {
+    attemptsLeft--
+    // console.log(attemptsLeft)
+
+    let actualPinArray = SECRET_PIN.split('')
+    let guessPinArray = guess.split('')
+
+    let correctPos = 0
+    let wrongPos = 0
+
+    let secretMatched = [false, false, false, false]
+    let guessMatched = [false, false, false, false]
+
+    for (let i = 0; i < 4; i++) {
+        if (guessPinArray[i] == actualPinArray[i]) {
+            correctPos++
+            secretMatched[i] = true
+            guessMatched[i] = true
+        }
+    }
+
+    for (let i = 0; i < 4; i++) {
+        if (!guessMatched[i]) {
+            for (let j = 0; j < 4; j++) {
+                if (!secretMatched[j] && guessPinArray[i] === actualPinArray[j]) {
+                    wrongPos++
+                    secretMatched[j] = true
+                    break
+                }
+            }
+        }
+    }
+
+    if (correctPos == 4) {
+        printToScreen('success')
+    } else if (attemptsLeft <= 0) {
+        printToScreen('no more attempts left')
+    } else {
+        printToScreen('try again')
+        printToScreen(`${correctPos} locked | ${wrongPos} vulnerable`)
+        // console.log(correctPos, wrongPos)
+    }
+}
