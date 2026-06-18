4fa2a9fefb61f3985366ac1de0810623443bc37f
strawberryskeleton <xradhi5685@gmail.com>
2026-06-18 22:33:48 +0530

    added better text outputs + some ascii art

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # encrypin
 code guessing game, completely text based
+
+
+console art: [https://www.asciiart.eu/art/24043b4588d6697f]
--- main.js
+++ main.js
@@ -16,7 +16,7 @@ userInput.addEventListener("keydown", (ev) => {
         let guess = userInput.value
         userInput.value = ''
 
-        printToScreen('> ' + guess)
+        printToScreen('> [your input] ' + guess)
 
 
         if (guess.toLowerCase() == 'clear') {
@@ -26,6 +26,7 @@ userInput.addEventListener("keydown", (ev) => {
 
         if (guess.toLowerCase() == 'help') {
             printToScreen("explanation of the terms + rules")
+            printToScreen(' ')
             return
         }
 
@@ -38,10 +39,12 @@ userInput.addEventListener("keydown", (ev) => {
         if (guess.length != 4 || isNaN(guess)) {
             // console.log('error')
             printToScreen('error')
+            printToScreen(' ')
             return
         }
 
         evaluateGuess(guess)
+        printToScreen('')
     }
 })
 
@@ -51,10 +54,37 @@ function printToScreen (text) {
 }
 
 function initaliseGame () {
-    printToScreen('Welcome to game')
-    printToScreen('Crack the code access the contents')
-    printToScreen('Input a 4 digit pin to start guessing, you have 10 attempts before the alarm starts ringing')
-    printToScreen('-------------------------------------------------------------------------------------------')
+    // printToScreen('Welcome to game')
+    // printToScreen('Crack the code access the contents')
+    // printToScreen('Input a 4 digit pin to start guessing, you have 10 attempts before the alarm starts ringing')
+    printToScreen('Crack The 4-digit Code to Find A Secret!')
+    printToScreen(`
+       ______________________________________________________________
+     .'  __________________________________________________________  '.
+     : .'                                                          '. :
+     | |      ________________________________________________      | |
+     | |    .:________________________________________________:.    | |
+     | |    |                                                  |    | |
+     | |    |                                                  |    | |
+     | |    |                Enter the PIN:                    |    | |
+     | |    |                                                  |    | |
+     | |    |                                                  |    | |
+     | |    |                [-] [-] [-] [-]                   |    | |
+     | |    |                                                  |    | |
+     | |    |                                                  |    | |
+     | |    |            __________________________            |    | |
+     | |    |           |  |  |  |  |  |  |  |  |  |           |    | |
+     | |    '.__________|__|__|__|__|__|__|__|__|__|__________.'    | |
+     | |                                                            | |
+     : '.__________________________________________________________.' :
+      ".____________________________________________________________."
+
+        `)
+    printToScreen('Available Commands:')
+    printToScreen(' help : show game rules and explanation')
+    printToScreen(' clear : clear the output console')
+    printToScreen(' hint : get a hint. but beware, the digit is randomly revealed and depends on your luck. it also costs you 2 attempts.')
+    printToScreen('----------------------------------------------------------------------------------------------------------------------')
     userInput.disabled = false
 }
 
@@ -94,21 +124,42 @@ function evaluateGuess (guess) {
     }
 
     if (correctPos == 4) {
-        printToScreen('success')
+        printToScreen('You Cracked the Code Successfully!!')
+        printToScreen('The GRAND SECRET IS....')
+        printToScreen('uh oh! file corrupted! the great grand secret cannot be revealed!')
+        printToScreen('----------------------------------------------------------------------------------------------------------------------')
+        printToScreen('----------------------------------------------------------------------------------------------------------------------')
+
         userInput.disabled = true
+
+        printToScreen('Reloading Game....')
+        printToScreen(' ')
         initaliseGame()
+
     } else if (attemptsLeft <= 0) {
-        printToScreen('no more attempts left')
+        printToScreen('No More Attempts Left!')
+        printToScreen('You could not crack the code. Now you will never know the grand secret.')
+        printToScreen(' ')
+        printToScreen('----------------------------------------------------------------------------------------------------------------------')
+        printToScreen('----------------------------------------------------------------------------------------------------------------------')
+
+        printToScreen('Reloading Game....')
+        printToScreen(' ')
+        initaliseGame()
+
     } else {
-        printToScreen('try again')
-        printToScreen(`${correctPos} locked | ${wrongPos} vulnerable`)
+        printToScreen('Incorrect PIN! Can not access the secret!')
+        printToScreen(`${correctPos} digits locked | ${wrongPos} digits vulnerable`)
+        printToScreen(' ')
         // console.log(correctPos, wrongPos)
     }
 }
 
 function giveHint () {
     if (attemptsLeft <= 3) {
-        printToScreen('hint denied. diy you fool')
+        printToScreen('Error!! You Do Not Have Enough Attempts To Take A Hint!')
+        printToScreen('You are on your own now. Crack the code or let it remain a mystery forever!')
+        printToScreen(' ')
         return
     }
 
@@ -117,7 +168,8 @@ function giveHint () {
     let randomIndex = Math.floor(Math.random() * 4)
     let revealedDigit = SECRET_PIN[randomIndex]
 
-    printToScreen('finr but it will cost you 2 attempts')
+    // printToScreen('finr but it will cost you 2 attempts')
     printToScreen(`${revealedDigit} is at position ${randomIndex + 1}`)
-    printToScreen(`remaining attempts: ${attemptsLeft}`)
+    printToScreen(`Remaining Attempts: ${attemptsLeft}`)
+    printToScreen(' ')
 }
